02ba4a4f03638900a8d325431d4c26d36e073f61
Imay Tri <imaytri.setiawan@gmail.com>
2019-07-22 22:35:52 +0700

    finish show species info

--- pokedex.go
+++ pokedex.go
@@ -6,117 +6,64 @@ import "strings"
 import "net/http"
 import "encoding/json"
 
-type pokemon struct {
-	Abilities []struct {
-		Ability struct {
-			Name string `json:"name"`
-			URL  string `json:"url"`
-		} `json:"ability"`
-		IsHidden bool `json:"is_hidden"`
-		Slot     int  `json:"slot"`
-	} `json:"abilities"`
-	BaseExperience int `json:"base_experience"`
-	Forms          []struct {
-		Name string `json:"name"`
-		URL  string `json:"url"`
-	} `json:"forms"`
-	GameIndices []struct {
-		GameIndex int `json:"game_index"`
-		Version   struct {
-			Name string `json:"name"`
-			URL  string `json:"url"`
-		} `json:"version"`
-	} `json:"game_indices"`
-	Height    int `json:"height"`
-	HeldItems []struct {
-		Item struct {
-			Name string `json:"name"`
-			URL  string `json:"url"`
-		} `json:"item"`
-		VersionDetails []struct {
-			Rarity  int `json:"rarity"`
-			Version struct {
-				Name string `json:"name"`
-				URL  string `json:"url"`
-			} `json:"version"`
-		} `json:"version_details"`
-	} `json:"held_items"`
-	ID                     int    `json:"id"`
-	IsDefault              bool   `json:"is_default"`
-	LocationAreaEncounters string `json:"location_area_encounters"`
-	Moves                  []struct {
-		Move struct {
-			Name string `json:"name"`
-			URL  string `json:"url"`
-		} `json:"move"`
-		VersionGroupDetails []struct {
-			LevelLearnedAt  int `json:"level_learned_at"`
-			MoveLearnMethod struct {
-				Name string `json:"name"`
-				URL  string `json:"url"`
-			} `json:"move_learn_method"`
-			VersionGroup struct {
-				Name string `json:"name"`
-				URL  string `json:"url"`
-			} `json:"version_group"`
-		} `json:"version_group_details"`
-	} `json:"moves"`
-	Name    string `json:"name"`
-	Order   int    `json:"order"`
-	Species struct {
-		Name string `json:"name"`
-		URL  string `json:"url"`
-	} `json:"species"`
-	Sprites struct {
-		BackDefault      string      `json:"back_default"`
-		BackFemale       interface{} `json:"back_female"`
-		BackShiny        string      `json:"back_shiny"`
-		BackShinyFemale  interface{} `json:"back_shiny_female"`
-		FrontDefault     string      `json:"front_default"`
-		FrontFemale      interface{} `json:"front_female"`
-		FrontShiny       string      `json:"front_shiny"`
-		FrontShinyFemale interface{} `json:"front_shiny_female"`
-	} `json:"sprites"`
-	Stats []struct {
-		BaseStat int `json:"base_stat"`
-		Effort   int `json:"effort"`
-		Stat     struct {
-			Name string `json:"name"`
-			URL  string `json:"url"`
-		} `json:"stat"`
-	} `json:"stats"`
-	Types []struct {
-		Slot int `json:"slot"`
-		Type struct {
-			Name string `json:"name"`
-			URL  string `json:"url"`
-		} `json:"type"`
-	} `json:"types"`
-	Weight int `json:"weight"`
-}
-
 func main(){
 	var q string
 	var q_url string
 	var err error
+	// var output string
 	poke := new(pokemon)
-
 	for i:=1; i<len(os.Args); i++ {
 		q = q + os.Args[i] + " "
-	} 
-	q_url = strings.ReplaceAll(strings.TrimSpace(strings.ToLower(q)), " ", "-")
-	fmt.Println(q_url)
-	fmt.Println(err)
-	fmt.Println()
+	}
+
+	q_url = changeStrFormat(q, true)
 
 	// call api
-	response, err := http.Get("https://pokeapi.co/api/v2/pokemon/ditto/")
+	response, err := http.Get("https://pokeapi.co/api/v2/pokemon/" + q_url)
 	if err != nil {
         fmt.Println(err)
     }
 	defer response.Body.Close()
 
 	err = json.NewDecoder(response.Body).Decode(&poke)
-	fmt.Println(poke.ID)
-	fmt.Println(poke.Name)
+	fmt.Printf("\n#%d - %s \n", poke.ID, changeStrFormat(poke.Name, false))
+	fmt.Printf("Type : ")
+	for i:=0; i<len(poke.Types); i++ {
+		fmt.Print(changeStrFormat(poke.Types[i].Type.Name, false))
+		if(i < len(poke.Types)-1){
+			fmt.Print(" - ")
+		} else {
+			fmt.Print("\n\nAbilities:\n")
+		} 
+	}
+
+	for i:=0; i<len(poke.Abilities); i++ {
+		fmt.Printf("%d. %s", i+1, poke.Abilities[i].Ability.Name)
+		var url string = poke.Abilities[i].Ability.URL
+		abi := new(ability)
+		response, err := http.Get(url)
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer response.Body.Close()
+		err = json.NewDecoder(response.Body).Decode(&abi)
+		for j:=0; j<len(abi.EffectEntries); j++ {
+			fmt.Printf("\n%s", abi.EffectEntries[j].Effect)
+		}
+
+		if(i<len(poke.Abilities)-1){
+			fmt.Print("\n\n")
+		}
+	}
+	
+}
+
+func changeStrFormat(str string, is_to_url bool) string {
+	var res string 
+	if is_to_url {
+		res = strings.ReplaceAll(strings.TrimSpace(strings.ToLower(str)), " ", "-")
+	} else {
+		res = strings.Title(strings.ReplaceAll(str, "-", " "))
+	}
+	return res
 }
---
+++ struct.go
@@ -0,0 +1,135 @@
+package main
+
+type pokemon struct {
+	Abilities []struct {
+		Ability struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"ability"`
+		IsHidden bool `json:"is_hidden"`
+		Slot     int  `json:"slot"`
+	} `json:"abilities"`
+	BaseExperience int `json:"base_experience"`
+	Forms          []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"forms"`
+	GameIndices []struct {
+		GameIndex int `json:"game_index"`
+		Version   struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"version"`
+	} `json:"game_indices"`
+	Height    int `json:"height"`
+	HeldItems []struct {
+		Item struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"item"`
+		VersionDetails []struct {
+			Rarity  int `json:"rarity"`
+			Version struct {
+				Name string `json:"name"`
+				URL  string `json:"url"`
+			} `json:"version"`
+		} `json:"version_details"`
+	} `json:"held_items"`
+	ID                     int    `json:"id"`
+	IsDefault              bool   `json:"is_default"`
+	LocationAreaEncounters string `json:"location_area_encounters"`
+	Moves                  []struct {
+		Move struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"move"`
+		VersionGroupDetails []struct {
+			LevelLearnedAt  int `json:"level_learned_at"`
+			MoveLearnMethod struct {
+				Name string `json:"name"`
+				URL  string `json:"url"`
+			} `json:"move_learn_method"`
+			VersionGroup struct {
+				Name string `json:"name"`
+				URL  string `json:"url"`
+			} `json:"version_group"`
+		} `json:"version_group_details"`
+	} `json:"moves"`
+	Name    string `json:"name"`
+	Order   int    `json:"order"`
+	Species struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"species"`
+	Sprites struct {
+		BackDefault      string      `json:"back_default"`
+		BackFemale       interface{} `json:"back_female"`
+		BackShiny        string      `json:"back_shiny"`
+		BackShinyFemale  interface{} `json:"back_shiny_female"`
+		FrontDefault     string      `json:"front_default"`
+		FrontFemale      interface{} `json:"front_female"`
+		FrontShiny       string      `json:"front_shiny"`
+		FrontShinyFemale interface{} `json:"front_shiny_female"`
+	} `json:"sprites"`
+	Stats []struct {
+		BaseStat int `json:"base_stat"`
+		Effort   int `json:"effort"`
+		Stat     struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"stat"`
+	} `json:"stats"`
+	Types []struct {
+		Slot int `json:"slot"`
+		Type struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"type"`
+	} `json:"types"`
+	Weight int `json:"weight"`
+}
+
+type ability struct {
+	EffectChanges []interface{} `json:"effect_changes"`
+	EffectEntries []struct {
+		Effect   string `json:"effect"`
+		Language struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"language"`
+		ShortEffect string `json:"short_effect"`
+	} `json:"effect_entries"`
+	FlavorTextEntries []struct {
+		FlavorText string `json:"flavor_text"`
+		Language   struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"language"`
+		VersionGroup struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"version_group"`
+	} `json:"flavor_text_entries"`
+	Generation struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"generation"`
+	ID           int    `json:"id"`
+	IsMainSeries bool   `json:"is_main_series"`
+	Name         string `json:"name"`
+	Names        []struct {
+		Language struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"language"`
+		Name string `json:"name"`
+	} `json:"names"`
+	Pokemon []struct {
+		IsHidden bool `json:"is_hidden"`
+		Pokemon  struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"pokemon"`
+		Slot int `json:"slot"`
+	} `json:"pokemon"`
+}
