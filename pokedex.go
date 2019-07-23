d15caf732d0f19b935dd464bca64f5c1a0e2792e
Imay Tri <imaytri.setiawan@gmail.com>
2019-07-23 11:31:28 +0700

    add show item, separate call api and print result as func, handle empty input, handle not found output

--- pokedex.go
+++ pokedex.go
@@ -6,56 +6,36 @@ import "strings"
 import "net/http"
 import "encoding/json"
 
+var poke = new(pokemon)
+var it = new(item)
+var test int
+
 func main(){
 	var q string
 	var q_url string
-	var err error
-	// var output string
-	poke := new(pokemon)
+	var stat int
+
 	for i:=1; i<len(os.Args); i++ {
 		q = q + os.Args[i] + " "
 	}
-
 	q_url = changeStrFormat(q, true)
+	if q_url=="" {
+		fmt.Println("Please fill Pokemon Species / Item parameter")
+		os.Exit(1)
+	}
 
-	// call api
-	response, err := http.Get("https://pokeapi.co/api/v2/pokemon/" + q_url)
-	if err != nil {
-        fmt.Println(err)
-    }
-	defer response.Body.Close()
-
-	err = json.NewDecoder(response.Body).Decode(&poke)
-	fmt.Printf("\n#%d - %s \n", poke.ID, changeStrFormat(poke.Name, false))
-	fmt.Printf("Type : ")
-	for i:=0; i<len(poke.Types); i++ {
-		fmt.Print(changeStrFormat(poke.Types[i].Type.Name, false))
-		if(i < len(poke.Types)-1){
-			fmt.Print(" - ")
+	// Process
+	stat, _ = callApi("https://pokeapi.co/api/v2/pokemon/" + q_url, poke)
+	if (stat != 404){
+		printResult("pokemon")
+	} else {
+		stat, _ = callApi("https://pokeapi.co/api/v2/item/" + q_url, it)
+		if (stat != 404){
+			printResult("item")		
 		} else {
-			fmt.Print("\n\nAbilities:\n")
-		} 
-	}
-
-	for i:=0; i<len(poke.Abilities); i++ {
-		fmt.Printf("%d. %s", i+1, poke.Abilities[i].Ability.Name)
-		var url string = poke.Abilities[i].Ability.URL
-		abi := new(ability)
-		response, err := http.Get(url)
-		if err != nil {
-			fmt.Println(err)
-		}
-		defer response.Body.Close()
-		err = json.NewDecoder(response.Body).Decode(&abi)
-		for j:=0; j<len(abi.EffectEntries); j++ {
-			fmt.Printf("\n%s", abi.EffectEntries[j].Effect)
-		}
-
-		if(i<len(poke.Abilities)-1){
-			fmt.Print("\n\n")
+			printResult("")
 		}
 	}
-	
 }
 
 func changeStrFormat(str string, is_to_url bool) string {
@@ -66,4 +46,50 @@ func changeStrFormat(str string, is_to_url bool) string {
 		res = strings.Title(strings.ReplaceAll(str, "-", " "))
 	}
 	return res
+}
+
+func callApi(url string, target interface{}) (int, error) {
+	response, err := http.Get(url)
+	if err != nil {
+        fmt.Println(err)
+    }
+	defer response.Body.Close()
+	return response.StatusCode, json.NewDecoder(response.Body).Decode(&target)
+}
+
+func printResult(opt string) {
+	switch opt {
+	case "pokemon" :
+		fmt.Printf("\n#%d - %s \n", poke.ID, changeStrFormat(poke.Name, false))
+		fmt.Printf("Type : ")
+		for i:=0; i<len(poke.Types); i++ {
+			fmt.Print(changeStrFormat(poke.Types[i].Type.Name, false))
+			if (i < len(poke.Types)-1){
+				fmt.Print(" - ")
+			} else {
+				fmt.Print("\n\nAbilities:\n")
+			} 
+		}
+
+		for i:=0; i<len(poke.Abilities); i++ {
+			fmt.Printf("%d. %s", i+1, changeStrFormat(poke.Abilities[i].Ability.Name, false))
+			var url string = poke.Abilities[i].Ability.URL
+			abi := new(ability)
+			callApi(url, abi)
+			for j:=0; j<len(abi.EffectEntries); j++ {
+				fmt.Printf("\n%s", abi.EffectEntries[j].Effect)
+			}
+
+			if (i<len(poke.Abilities)-1){
+				fmt.Print("\n\n")
+			}
+		}
+	case "item": 
+		fmt.Printf("\nItem : %s\nCost : %d\n", changeStrFormat(it.Name, false), it.Cost)
+		for i:=0; i<len(it.EffectEntries); i++ {
+			fmt.Printf("Entries : %s ", it.EffectEntries[i].Effect)
+		}
+	default:
+		fmt.Println("Pokemon Species or Item not found.")
+	}
 }
--- struct.go
+++ struct.go
@@ -133,3 +133,55 @@ type ability struct {
 		Slot int `json:"slot"`
 	} `json:"pokemon"`
 }
+
+type item struct {
+	Attributes     []interface{} `json:"attributes"`
+	BabyTriggerFor interface{}   `json:"baby_trigger_for"`
+	Category       struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"category"`
+	Cost          int `json:"cost"`
+	EffectEntries []struct {
+		Effect   string `json:"effect"`
+		Language struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"language"`
+		ShortEffect string `json:"short_effect"`
+	} `json:"effect_entries"`
+	FlavorTextEntries []struct {
+		Language struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"language"`
+		Text         string `json:"text"`
+		VersionGroup struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"version_group"`
+	} `json:"flavor_text_entries"`
+	FlingEffect interface{} `json:"fling_effect"`
+	FlingPower  interface{} `json:"fling_power"`
+	GameIndices []struct {
+		GameIndex  int `json:"game_index"`
+		Generation struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"generation"`
+	} `json:"game_indices"`
+	HeldByPokemon []interface{} `json:"held_by_pokemon"`
+	ID            int           `json:"id"`
+	Machines      []interface{} `json:"machines"`
+	Name          string        `json:"name"`
+	Names         []struct {
+		Language struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"language"`
+		Name string `json:"name"`
+	} `json:"names"`
+	Sprites struct {
+		Default string `json:"default"`
+	} `json:"sprites"`
+}
